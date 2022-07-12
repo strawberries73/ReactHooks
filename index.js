ae18c39bdd657966ab0f711ca05d7b4223285191
Jenn <39388566+strawberries73@users.noreply.github.com>
2022-07-12 11:24:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [
+  ...Array(length)
+];
+function Star({ selected = false, onSelect }) {
+  return (
+  <FaStar 
+    color={selected ? "red" : "gray"} 
+    onClick={onSelect}
+  />
+  );
+}
+function StarRating({ totalStars = 5 }) {
+  const [
+    selectedStars, 
+    setSelectedStars
+  ] = useState(0);
+  return <>
+  {(createArray(totalStars).map((n, i) => 
+  <Star 
+    key={i} 
+    selected= {selectedStars > i} 
+    onSelect={() => setSelectedStars(i + 1)}
+        />
+  ))}
+  <p>{selectedStars} of {totalStars}</p>
+  </>
+}
+function App() {
+  return <StarRating totalStars={5}/>;
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
